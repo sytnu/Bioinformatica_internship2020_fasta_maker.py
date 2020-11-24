99c64fd47bc1e827280c0b2ee7b929b4ec1eb674
Kannie Yuan <kyuan410@gmail.com>
2020-11-24 22:50:47 +0000

    I added comments.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+#import random to be able to pull random sequences 
+#import io helps for data extraction and IOError
 import random 
 import io
-
+#nucleo_list is our variable name for our sequences, which are the DNA sequences AGCT
 nucleo_list = ("A","G","C","T")
-
+#DNA_sequence is definied with these variables since it will help us select random sequences and then we are able to combine them
 def DNA_sequence():
   subset1 = ''
   subset2 = ''
@@ -12,12 +14,12 @@ def DNA_sequence():
     subset2 += random.choice(nucleo_list)
   full_sequence = subset1 + subset2 + subset1 + subset2
   return(full_sequence)
-
+#spidroin is same value as DNA_sequence but named
 spidroin = DNA_sequence()
-
+#this will help us create new files 
 filename = input("File name is: ")
 file_header = input("File Header is:")
-
+#this will create fasta files and check for errors
 def spidroinFile(filename, file_header):
   try:
     with open(filename + ".fasta", "w") as spidroinFile_handle:
